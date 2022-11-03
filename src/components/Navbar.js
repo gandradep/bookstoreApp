978d6f9f9a836ffc0fe17c00030088748e134520
gandradep <eventsonido@gmail.com>
2022-11-03 17:02:08 +0100

    Setting up files

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends React.PureComponent {
+  render() {
+    return (
+      <nav className="dFlex nav">
+        <h1 className="fsizeBig fw400"> Bookstore CMS </h1>
+
+      </nav>
+    );
+  }
+}
+export default Navbar;
